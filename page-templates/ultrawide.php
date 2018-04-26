018ea1a1e7c2c1db169ee52737b3b17c87d70138
Benjamin Kampmann <ben@gnunicorn.org>
2018-04-26 15:46:30 +0200

    add ultrawide page layout, split out scss into multi-files

---
+++ page-templates/ultrawide.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template Name: Ultrawide Page
+ *
+ * Template for displaying a full page  with lots of whitespace and big texts
+ *
+ * @package understrap
+ */
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<div class="wrapper" id="full-width-page-wrapper">
+
+	<div id="content">
+
+		<main class="site-main container-fluid ultrawide" id="main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'loop-templates/content', 'page' ); ?>
+
+				<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+
+					comments_template();
+
+				endif;
+				?>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+
+	</div><!-- Container end -->
+
+</div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
