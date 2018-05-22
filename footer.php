c140db10616fec10042f7b47bf9199ea64d4dcac
Benjamin Kampmann <ben@gnunicorn.org>
2018-05-22 14:59:49 +0200

    mautic tracks pageviews, too

--- footer.php
+++ footer.php
@@ -273,8 +273,15 @@ jQuery(function($) {
     head.appendChild(script);
 });
 </script>
-<script data-src="https://mautic.bewegung.jetzt/mtc.js" data-cookieconsent="required"></script>
 
+<script type="text/plain">
+    (function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
+        w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
+        m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://mautic.bewegung.jetzt/mtc.js','mt');
+
+    mt('send', 'pageview');
+</script>
 
 <!-- Matomo -->
 <script type="text/javascript">
