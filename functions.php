2cba8c016b860637f61dcbc4d2d812aba48781b3
Malte Muens <m.muens@muensmedia.de>
2018-08-24 13:26:24 +0200

    Style WordPress login v3

--- functions.php
+++ functions.php
@@ -73,7 +73,7 @@ add_action( 'after_setup_theme', 'add_child_theme_textdomain' );
  */
 function dib_login_logo() { ?>
 	<style type="text/css">
-		body.login{ background-image: url(https://wolke.bewegung.jetzt/apps/theming/loginbackground?v=17); background-color: #274f9e; }
+		body.login{ background-image: url(https://wolke.bewegung.jetzt/apps/theming/loginbackground?v=17); background-color: #274f9e; background-position: center; background-size: cover; }
 		.login h1 a { background-image: url(https://wolke.bewegung.jetzt/apps/theming/logo?v=17) !important; background-size: 150px !important; height: 150px !important; width: 150px !important; }
 		.login #nav { color: #fff; !important; }
 		.login #backtoblog a, .login #nav a { color: #fff; }
